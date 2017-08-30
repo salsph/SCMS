f2bf2377de029ccacd32742d089c2ee70b550f08
Mikita <sal_sp.h@mail.ru>
2017-08-30 22:32:36 +0300

    Start .

---
+++ SCMS/engine/DI/DI.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Никитка
+ * Date: 30.08.2017
+ * Time: 19:51
+ */
+
+namespace Engine\Di;
+
+/**
+ * Class DI
+ * @package Engine\Di
+ */
+class DI
+{
+    /**
+     * @var array
+     */
+    private $container = [];
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function __set($key, $value){
+        $this->container['$key'] = $value;
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function __get($key){
+        return $this->container['$key'] ? isset($this->container['$key']) : false;
+    }
+
+
+
+
+}
---
+++ SCMS/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Никитка
+ * Date: 30.08.2017
+ * Time: 19:49
+ */
